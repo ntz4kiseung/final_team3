8dd6d67162a06defb4b9b3456c3800a637ea80f5
wing4465 <hkwcho1993@gmail.com>
2019-07-15 01:04:29 +0900

    20190715/오전 1시 03분 커밋
    업데이트 가능 (몇가지 부분 손봐야됨)

--- src/com/test/mybatis/IInterDAO.java
+++ src/com/test/mybatis/IInterDAO.java
@@ -2,6 +2,8 @@ package com.test.mybatis;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface IInterDAO
 {
 	public ArrayList<InterDTO> interMainList();
@@ -9,5 +11,10 @@ public interface IInterDAO
 	public int userInterInsert1(InterDTO inter);
 	public int userInterInsert2(InterDTO inter);
 	public int userInterInsert3(InterDTO inter);
+
+	public int updateInter1(@Param("userId")String userId, @Param("inter")InterDTO inter, @Param("inter2")InterDTO inter2);
+	public int updateInter2(@Param("userId")String userId, @Param("inter")InterDTO inter, @Param("inter2")InterDTO inter2);
+	public int updateInter3(@Param("userId")String userId, @Param("inter")InterDTO inter, @Param("inter2")InterDTO inter2);
+	
 
 }
